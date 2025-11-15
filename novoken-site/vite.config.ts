d81330b5a7d549b6032ce3b1b21dcbd8e302b9ff
aayan-baig <googling32123@gmail.com>
2025-11-15 16:52:35 +0530

    docs dir

--- novoken-site/vite.config.ts
+++ novoken-site/vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",          // change later if repo name hosting, for now fine
+  base: "/scudio_src/",          // change later if repo name hosting, for now fine
   build: {
-    outDir: "docs"    // GitHub Pages will serve /docs
+    outDir: "../docs",    // GitHub Pages will serve /docs
+    emptyOutDir: false,
   }
 });
